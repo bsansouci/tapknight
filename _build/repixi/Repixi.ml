52e7269294e9e3c7d2b67d59af4744500143f10a
Benjamin San Souci <benjamin.sansouci@gmail.com>
2016-09-13 03:17:36 -0700

    Remove build. No more heroku for now.

--- _build/repixi/Repixi.ml
+++
@@ -1,7 +0,0 @@
-module Demo1 = Repixi__Demo1
-module Demo2 = Repixi__Demo2
-module Demo3 = Repixi__Demo3
-module Demo4 = Repixi__Demo4
-module Demo5 = Repixi__Demo5
-module Index = Repixi__Index
-module Repixi = Repixi__Repixi
